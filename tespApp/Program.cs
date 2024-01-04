7ba7369a3b118ceeab2feef649b50ba8f98ecdf6
githubDlyaHuesosovI314Idarasov <thepokemontrainergold@gmail.com>
2024-01-04 09:19:51 +0200

    change 1

---
+++ tespApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tespApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye world!");
+            Console.ReadLine();
+
+            Console.WriteLine("Changes here");
+            Console.WriteLine("Change here another");
+        }
+    }
+}
